147e8837ad162dcbd1d8ba38059aeac1c17e150d
jmcmains <jmcmains@gmail.com>
2012-11-14 13:25:01 -0500

    added paypal receipt display

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,4 +3,8 @@ class StaticPagesController < ApplicationController
  		@title ="RubberBanditz Inventory Launching Page"
  		@subtitle= "What would you like to do?"
   end
+  
+  def paypal
+  	redirect_to "https://www.paypal.com/us/vst/id=#{params[:Order_ID]}"
+  end
 end
